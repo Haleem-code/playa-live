388d7502feaf5b6bc09c5f295f09c07573ff4fe7
unknown <abdulhaleemayomide@gmail.com>
2026-01-24 12:23:39 +0100

    fix layout build error

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
+'use client';
+
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { Header } from '@/components/layout/Header';
 import { Sidebar } from '@/components/layout/Sidebar';
